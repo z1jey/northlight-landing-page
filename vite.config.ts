07d663f175713b10086e8ff28fa5995b642b1322
Christine Batallones <christinebatallones0@gmail.com>
2026-08-16 02:10:15 +0800

    setup shadcn and its reusable components

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -13,4 +14,9 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
